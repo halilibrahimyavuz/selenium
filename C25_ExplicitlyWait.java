7606e865d61f964ec16aa7a8f1b3f01f60884d68
Halil İbrahim Yavuz <halilibrahimyavuz07@gmail.com>
2022-02-07 12:18:05 +0300

    TestNG

---
+++ C25_ExplicitlyWait.java
@@ -0,0 +1,18 @@
+package tests.day13;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class C07_ExplicitlyWait extends TestBase {
+
+    @Test
+    public void test01(){
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(50));
+      //  wait.until(ExpectedCondition.)
+    }
+}
